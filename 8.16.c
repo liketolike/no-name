b7db3d7ff68718e17eccb60c5f2d3b1110991c83
liketolike <vs2019yyds@outlook.com>
2021-09-03 18:05:26 +0800

    bad

---
+++ 8.16.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+double FindAvarage(double a[10])
+{
+	int b;
+	double avarage;
+	double sum = FindAvarage[0];
+
+	for (b = 0; b < 10; b += 1)
+	{
+		sum += FindAvarage[b];
+	}
+	avarage = sum / 10;
+	return avarage;
+
+}
+
+int main(void)
+{
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
